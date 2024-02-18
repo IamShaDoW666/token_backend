9eac62bcf41b625aa34ad7c45b7bfcfbed29a3e7
IamShaDoW666 <www.gaming2018@gmail.com>
2024-02-18 22:15:48 +0530

    add gitignore

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -95,11 +95,11 @@ class TokenController extends Controller
         $options->set('isPhpEnabled', true);
         $pdf = new Dompdf($options);
         $pdf = Pdf::loadView('pdf.template', ['queue' => $queue, 'customer_waiting' => $customer_waiting, 'settings' => $settings]);
-        $tempPdfPath = public_path('temp-pdfs/' . $queue->number . '.pdf');
+        $tempPdfPath = public_path('temp-pdfs/' . $queue->id . '.pdf');
         $pdf->save($tempPdfPath);
 
-        SendWhatsappJob::dispatchAfterResponse($queue, $customer_waiting, asset('temp-pdfs/' . $queue->number . '.pdf'), $service->optin_message_format, $settings, 'issue_token');
-        return response()->json(['status_code' => 200, 'queue' => $queue, 'customer_waiting' => $customer_waiting, 'settings' => $settings, 'asset' => asset('temp-pdfs/' . $queue->number . '.pdf')]);
+        SendWhatsappJob::dispatch($queue, $customer_waiting, asset('temp-pdfs/' . $queue->id . '.pdf'), $service->optin_message_format, $settings, 'issue_token');
+        return response()->json(['status_code' => 200, 'queue' => $queue, 'customer_waiting' => $customer_waiting, 'settings' => $settings, 'asset' => asset('temp-pdfs/' . $queue->id . '.pdf')]);
     }
     public function liveToken(Queue $queue)
     {
