a1b637df40fb2c306a6a4ae306fad8827f7cf48b
IamShaDoW666 <www.gaming2018@gmail.com>
2024-02-18 23:09:32 +0530

    whatsapp

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -96,9 +96,11 @@ class TokenController extends Controller
         $pdf = new Dompdf($options);
         $pdf = Pdf::loadView('pdf.template', ['queue' => $queue, 'customer_waiting' => $customer_waiting, 'settings' => $settings]);
         $tempPdfPath = public_path('temp-pdfs/' . $queue->id . '.pdf');
+        
         $pdf->save($tempPdfPath);
         $pdfPath = asset('temp-pdfs/' . $queue->id . '.pdf');
-        SendWhatsappJob::dispatchAfterResponse($queue, $customer_waiting, $pdfPath , $service->optin_message_format, $settings, 'issue_token');
+        
+        SendWhatsappJob::dispatchAfterResponse($queue, $pdfPath , $service->optin_message_format, $settings, 'issue_token');
         return response()->json(['status_code' => 200, 'queue' => $queue, 'customer_waiting' => $customer_waiting, 'settings' => $settings, 'asset' => asset('temp-pdfs/' . $queue->id . '.pdf')]);
     }
     public function liveToken(Queue $queue)
